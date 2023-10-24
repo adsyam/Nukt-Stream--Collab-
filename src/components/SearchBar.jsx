50f6544c4ddee7229388433c056507b3d199e918
danemaas <danemaas16@gmail.com>
2023-10-24 08:06:18 +0800

    fixed Search_Media query

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,18 @@
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { motion } from "framer-motion"
-import { useState } from "react"
-import { Link, useNavigate, useLocation } from "react-router-dom"
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 export const Searchbar = () => {
-  const [search, setSearch] = useState("")
-  const location = useLocation()
-  const pathname = location.pathname
+  const [search, setSearch] = useState("");
+  const location = useLocation();
+  const pathname = location.pathname;
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    window.location.href = `/search?q=${search}`
-  }
+    e.preventDefault();
+    window.location.href = `/search?q=${search}`;
+  };
 
   const searchStyle = {
     backgroundColor: "rgba(0, 0, 0, 0.1)",
@@ -22,14 +22,14 @@ export const Searchbar = () => {
     color: "white",
     border: "1px solid white",
     minWidth: "300px",
-  }
-  const navigate = useNavigate()
+  };
+  const navigate = useNavigate();
 
   function searchEnter(e) {
     if (search !== "") {
       if (e.key === "Enter") {
-        navigate(`/search/${search}`)
-        setSearch("")
+        navigate(`/search?q=${search}`);
+        setSearch("");
       }
     }
   }
@@ -56,7 +56,7 @@ export const Searchbar = () => {
       ></Link> */}
       <Link
         className="text-white absolute pr-3"
-        to={search !== "" ? `/search/${search}` : pathname}
+        to={search !== "" ? `/search?q=${search}` : pathname}
         onClick={() => setSearch("")}
       >
         <motion.div whileHover={{ scale: 1.15 }} className="">
@@ -65,5 +65,5 @@ export const Searchbar = () => {
       </Link>
       {/* </form> */}
     </div>
-  )
-}
+  );
+};
