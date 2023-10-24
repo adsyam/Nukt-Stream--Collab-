c71813b20399e6a32fa33552864c0b0d658b885b
adsyam <yambaoadrianne@gmail.com>
2023-10-24 19:08:21 +0800

    index imports

--- src/components/index.js
+++ src/components/index.js
@@ -8,48 +8,52 @@ import { Footer } from "./Footer"
 import { ForgotPassword } from "./ForgotPassword"
 // import { Forms } from "./Forms"
 // import { Home } from "../pages/Home"
-import { SearchPage } from "../pages/SearchPage";
-import { Hero } from "./Hero";
-import { History } from "./History";
-import { Login } from "./Login";
-import { Navbar } from "./Navbar";
-import { Plans } from "./Plans";
-import { Pricing } from "./Pricing";
-import { ProfileDetails } from "./ProfileDetails";
-import { ProfileNav } from "./ProfileNav";
-import { ProfilePic } from "./ProfilePic";
-import { Profile_About } from "./Profile_About";
-import { Profile_Contents } from "./Profile_Contents";
-import { Profile_Downloads } from "./Profile_Downloads";
-import { Profile_Home } from "./Profile_Home";
-import { Profile_Playlist } from "./Profile_Playlist";
-import { ProtectedRoute } from "./ProtectedRoute";
-import { Reviews } from "./Reviews";
-import { Searchbar } from "./SearchBar";
-import { Sidebar } from "./Sidebar";
-import { SignUp } from "./SignUp";
-import { Success } from "./Success";
-import { User } from "./User";
-import { UserSidebar } from "./UserSideBar";
-import { VideoCard } from "./VideoCard";
-import { VideoCategories } from "./VideoCategories";
-import { VideoDescriptions } from "./VideoDescription";
-import { VideoFeed } from "./VideoFeed";
-import { VideosGrid } from "./VideosGrid";
-import { VideosLinear } from "./VideosLinear";
+import { SearchPage } from "../pages/SearchPage"
+import { Hero } from "./Hero"
+import { History } from "./History"
+import { Login } from "./Login"
+import { Navbar } from "./Navbar"
+import { Plans } from "./Plans"
+import { Pricing } from "./Pricing"
+import { ProfileDetails } from "./ProfileDetails"
+import { ProfileNav } from "./ProfileNav"
+import { ProfilePic } from "./ProfilePic"
+import { Profile_About } from "./Profile_About"
+import { Profile_Contents } from "./Profile_Contents"
+import { Profile_Downloads } from "./Profile_Downloads"
+import { Profile_Home } from "./Profile_Home"
+import { Profile_Playlist } from "./Profile_Playlist"
+import { ProtectedRoute } from "./ProtectedRoute"
+import { Reviews } from "./Reviews"
+import { Searchbar } from "./SearchBar"
+import { Sidebar } from "./Sidebar"
+import { SignUp } from "./SignUp"
+import { Success } from "./Success"
+import { User } from "./User"
+import { UserSidebar } from "./UserSideBar"
+import { VideoCard } from "./VideoCard"
+import { VideoCategories } from "./VideoCategories"
+import { VideoDescriptions } from "./VideoDescription"
+import { VideoFeed } from "./VideoFeed"
+import { VideosGrid } from "./VideosGrid"
+import { VideosLinear } from "./VideosLinear"
 // import { WatchPage } from "./WatchPage"
-import Carousel from "./Carousel";
-import GenreMap from "./GenreMap";
-import MediaDetails from "./MediaDetails";
-import MediaFrame from "./MediaFrame";
-import MediaReviews from "./MediaReviews";
-import Popular from "./Popular";
-import SearchMovie from "./SearchMovie";
-import SearchTVSeries from "./SearchTVSeries";
-import TopRated from "./TopRated";
-import Trending from "./Trending";
-import EpisodeList from "./EpisodeList";
-import ServerList from "./ServerList.jsx";
+import Carousel from "./Carousel"
+import EpisodeList from "./EpisodeList"
+import GenreMap from "./GenreMap"
+import MediaDetails from "./MediaDetails"
+import MediaFrame from "./MediaFrame"
+import MediaRecommendation from "./MediaRecommendation"
+import MediaReviews from "./MediaReviews"
+import { MovieWatchHistory } from "./MovieWatchHistory"
+import Popular from "./Popular"
+import { RedirectRoute } from "./RedirectRoute"
+import SearchMovie from "./SearchMovie"
+import SearchTVSeries from "./SearchTVSeries"
+import { SeriesWatchHistory } from "./SeriesWatchHistory"
+import ServerList from "./ServerList.jsx"
+import TopRated from "./TopRated"
+import Trending from "./Trending"
 
 export {
   Banner,
@@ -70,7 +74,9 @@ export {
   Login,
   MediaDetails,
   MediaFrame,
+  MediaRecommendation,
   MediaReviews,
+  MovieWatchHistory,
   Navbar,
   Plans,
   Popular,
@@ -90,6 +96,7 @@ export {
   SearchPage,
   SearchTVSeries,
   Searchbar,
+  SeriesWatchHistory,
   ServerList,
   Sidebar,
   SignUp,
@@ -104,6 +111,4 @@ export {
   VideoFeed,
   VideosGrid,
   VideosLinear,
-  MovieWatchHistory,
-  SeriesWatchHistory,
-};
+}
