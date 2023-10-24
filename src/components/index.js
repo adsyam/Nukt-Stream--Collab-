b8ac90330d8a22020c77fbb1a051e06f3c7864d3
adsyam <yambaoadrianne@gmail.com>
2023-10-24 18:29:39 +0800

    added route

--- src/components/index.js
+++ src/components/index.js
@@ -1,57 +1,58 @@
-import { Banner } from "./Banner";
+import { Banner } from "./Banner"
 // import { Buttons } from "./Buttons"
-import { ChannelCard } from "./ChannelCard";
-import { CoverPhoto } from "./CoverPhoto";
-import { Dashboard } from "./Dashboard";
-import { FAQs } from "./FAQs";
-import { Footer } from "./Footer";
-import { ForgotPassword } from "./ForgotPassword";
+import { ChannelCard } from "./ChannelCard"
+import { CoverPhoto } from "./CoverPhoto"
+import { Dashboard } from "./Dashboard"
+import { FAQs } from "./FAQs"
+import { Footer } from "./Footer"
+import { ForgotPassword } from "./ForgotPassword"
 // import { Forms } from "./Forms"
 // import { Home } from "../pages/Home"
-import { SearchPage } from "../pages/SearchPage";
-import { Hero } from "./Hero";
-import { History } from "./History";
-import { Login } from "./Login";
-import { Navbar } from "./Navbar";
-import { Plans } from "./Plans";
-import { Pricing } from "./Pricing";
-import { ProfileDetails } from "./ProfileDetails";
-import { ProfileNav } from "./ProfileNav";
-import { ProfilePic } from "./ProfilePic";
-import { Profile_About } from "./Profile_About";
-import { Profile_Contents } from "./Profile_Contents";
-import { Profile_Downloads } from "./Profile_Downloads";
-import { Profile_Home } from "./Profile_Home";
-import { Profile_Playlist } from "./Profile_Playlist";
-import { ProtectedRoute } from "./ProtectedRoute";
-import { Reviews } from "./Reviews";
-import { Searchbar } from "./SearchBar";
-import { Sidebar } from "./Sidebar";
-import { SignUp } from "./SignUp";
-import { Success } from "./Success";
-import { User } from "./User";
-import { UserSidebar } from "./UserSideBar";
-import { VideoCard } from "./VideoCard";
-import { VideoCategories } from "./VideoCategories";
-import { VideoDescriptions } from "./VideoDescription";
-import { VideoFeed } from "./VideoFeed";
-import { VideosGrid } from "./VideosGrid";
-import { VideosLinear } from "./VideosLinear";
-import { MovieWatchHistory } from "./MovieWatchHistory";
-import { SeriesWatchHistory } from "./SeriesWatchHistory";
+import { SearchPage } from "../pages/SearchPage"
+import { Hero } from "./Hero"
+import { History } from "./History"
+import { Login } from "./Login"
+import { MovieWatchHistory } from "./MovieWatchHistory"
+import { Navbar } from "./Navbar"
+import { Plans } from "./Plans"
+import { Pricing } from "./Pricing"
+import { ProfileDetails } from "./ProfileDetails"
+import { ProfileNav } from "./ProfileNav"
+import { ProfilePic } from "./ProfilePic"
+import { Profile_About } from "./Profile_About"
+import { Profile_Contents } from "./Profile_Contents"
+import { Profile_Downloads } from "./Profile_Downloads"
+import { Profile_Home } from "./Profile_Home"
+import { Profile_Playlist } from "./Profile_Playlist"
+import { ProtectedRoute } from "./ProtectedRoute"
+import { Reviews } from "./Reviews"
+import { Searchbar } from "./SearchBar"
+import { SeriesWatchHistory } from "./SeriesWatchHistory"
+import { Sidebar } from "./Sidebar"
+import { SignUp } from "./SignUp"
+import { Success } from "./Success"
+import { User } from "./User"
+import { UserSidebar } from "./UserSideBar"
+import { VideoCard } from "./VideoCard"
+import { VideoCategories } from "./VideoCategories"
+import { VideoDescriptions } from "./VideoDescription"
+import { VideoFeed } from "./VideoFeed"
+import { VideosGrid } from "./VideosGrid"
+import { VideosLinear } from "./VideosLinear"
 // import { WatchPage } from "./WatchPage"
-import Carousel from "./Carousel";
-import GenreMap from "./GenreMap";
-import MediaDetails from "./MediaDetails";
-import MediaFrame from "./MediaFrame";
-import MediaReviews from "./MediaReviews";
-import Popular from "./Popular";
-import SearchMovie from "./SearchMovie";
-import SearchTVSeries from "./SearchTVSeries";
-import TopRated from "./TopRated";
-import Trending from "./Trending";
-import EpisodeList from "./EpisodeList";
-import ServerList from "./ServerList.jsx";
+import Carousel from "./Carousel"
+import EpisodeList from "./EpisodeList"
+import GenreMap from "./GenreMap"
+import MediaDetails from "./MediaDetails"
+import MediaFrame from "./MediaFrame"
+import MediaReviews from "./MediaReviews"
+import Popular from "./Popular"
+import { RedirectRoute } from "./RedirectRoute"
+import SearchMovie from "./SearchMovie"
+import SearchTVSeries from "./SearchTVSeries"
+import ServerList from "./ServerList.jsx"
+import TopRated from "./TopRated"
+import Trending from "./Trending"
 
 export {
   Banner,
@@ -60,6 +61,7 @@ export {
   ChannelCard,
   CoverPhoto,
   Dashboard,
+  EpisodeList,
   FAQs,
   Footer,
   ForgotPassword,
@@ -72,6 +74,7 @@ export {
   MediaDetails,
   MediaFrame,
   MediaReviews,
+  MovieWatchHistory,
   Navbar,
   Plans,
   Popular,
@@ -85,11 +88,14 @@ export {
   Profile_Home,
   Profile_Playlist,
   ProtectedRoute,
+  RedirectRoute,
   Reviews,
   SearchMovie,
   SearchPage,
   SearchTVSeries,
   Searchbar,
+  SeriesWatchHistory,
+  ServerList,
   Sidebar,
   SignUp,
   Success,
@@ -103,8 +109,4 @@ export {
   VideoFeed,
   VideosGrid,
   VideosLinear,
-  EpisodeList,
-  ServerList,
-  MovieWatchHistory,
-  SeriesWatchHistory,
-};
+}
