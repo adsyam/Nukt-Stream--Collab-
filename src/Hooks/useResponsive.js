cd0791cc0f1f49598998e31c86a5028937af86d3
adsyam <yambaoadrianne@gmail.com>
2023-11-03 20:53:08 +0800

    temp

--- src/Hooks/useResponsive.js
+++ src/Hooks/useResponsive.js
@@ -17,13 +17,6 @@ export default function useResponsive() {
     "grid grid-cols-8 max-xl:grid-cols-7 max-lg:grid-cols-6 max-md:grid-cols-5 mx-32 max-lg:mx-20 max-sm:mx-12 max-sm:grid-cols-4 max-xsm:grid-cols-3 max-xxsm:grid-cols-2 gap-4 text-white"
 
   useEffect(() => {
-    if (sidebar) {
-      if (md) setMaxCards(10)
-      else if (lg) setMaxCards(12)
-      else if (xl) setMaxCards(14)
-      else if (xxl) setMaxCards(14)
-      else setMaxCards(6)
-    } else if (!sidebar) {
       if (xxsm) setMaxCards(9)
       else if (xsm) setMaxCards(8)
       else if (sm) setMaxCards(10)
@@ -32,7 +25,6 @@ export default function useResponsive() {
       else if (xl) setMaxCards(16)
       else if (xxl) setMaxCards(16)
       else setMaxCards(6)
-    }
   }, [xxsm, xsm, sm, md, lg, xl, xxl, sidebar])
 
   return { maxCards, setMaxCards, responsiveGridCard, screen }
